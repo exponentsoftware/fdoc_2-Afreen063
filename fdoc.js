e46ca8e1ea7949e53a8374af442e302b8f636bd8
Afreen063 <afreen0501@gmail.com>
2022-09-27 22:59:52 +0530

    last two question left

--- fdoc.js
+++ fdoc.js
@@ -37,3 +37,88 @@ function createArrayOfArrays(arr)
 }
 
   createArrayOfArrays(countries)
+
+
+  //2
+
+
+  const users = [
+	{
+		name:'Brook', 
+		scores:75,
+		skills:['HTM', 'CSS', 'JS'],
+		age:16
+	},
+	{
+		name:'Alex', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	}, 
+	{
+		name:'David', 
+		scores:75,
+		skills:['HTM', 'CSS'],
+		age:22
+	}, 
+	{
+		name:'John', 
+		scores:85,
+		skills:['HTM'],
+		age:25
+	},
+	{
+		name:'Sara',
+		scores:95,
+		skills:['HTM', 'CSS', 'JS'],
+		age: 26
+	},
+	{
+		name:'Martha', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	},
+	{
+		name:'Thomas',
+		scores:90,
+		skills:['HTM', 'CSS', 'JS'],
+		age:20
+	}
+	];
+
+    function scoresGreaterThan85(user)
+    {
+         let result=  user.filter((num)=>{
+            if(num.scores>85)
+            {
+                console.log(num.name)
+                return num.name;
+            }
+           })
+            console.log(result);
+    }
+    scoresGreaterThan85(users)
+
+    function addUser(users, newUser)
+    {
+             let check=0;
+             users.map((e)=>{
+                if(e.name==newUser)
+                {
+                    check++;
+                }
+             });
+             if(check==0)
+             {
+                users.push(newUser);
+                console.log(users)
+             }
+    }
+    const usersdata={
+        name:'Afreen', 
+		scores:75,
+		skills:['HTM', 'CSS', 'JS'],
+		age:16
+    }
+    addUser(users,usersdata)
